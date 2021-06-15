1259ce4a23787d8bb7137daf28e9b7ef0604a35b
Vibhu pandey <vibhupandey.vp2gmail.com>
2021-06-16 00:02:41 +0530

    commenting

--- index.js
+++ index.js
@@ -118,7 +118,7 @@ const adminHelp = (prefix, groupName) => {
     _Show Tech News_
     
 *${prefix}spam*
-    _spamming feature testing phase_    
+    _spamming feature testing phase_(testing  phase)   
 
 *${prefix}removebot*
     _Remove bot from group!_
@@ -486,7 +486,11 @@ async function main() {
 
 
                 /////////////// ADMIN COMMANDS \\\\\\\\\\\\\\\
-                case 'spam':
+                case 'rules':
+                    if (!isGroup) return;
+                    reply('Its in testing phase,give us some time')
+                
+                /*case 'spam':
                     if (!isGroup) return;
                     if ((msg.body.startsWith("/spam ")) && (msg.fromMe)) { // Spamming Op in the chat
             var i, count
@@ -537,7 +541,7 @@ async function main() {
                 for (i = 0; i < count; i++)
                     client.sendMessage(msg.to, text)
             }
-        }         
+        } */        
                 case 'news':
                     if (!isGroup) return;
                     if (!isGroupAdmins) {
