c44514f14f4cac618f1ceacab0cf4ca21baae53d
Vibhu pandey <vibhupandey.vp2gmail.com>
2021-07-12 23:42:58 +0530

    ladai

--- index.js
+++ index.js
@@ -182,7 +182,7 @@ async function getPrice(cryptoCode) {
     return axios(mainconfig)
         .then(async function (response) {
             var data = response.data
-            $console.log(data)
+            console.log(data)
             var cryptoCodeINR = cryptoCode + "INR"
             if (data[cryptoCode.toString()] != undefined || data[cryptoCodeINR.toString()] != undefined) {
                 cryptoCode = data[cryptoCode] == undefined ? cryptoCodeINR : cryptoCode
