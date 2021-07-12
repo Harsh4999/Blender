6689e6394eeaa102b1eeddad7f284ff04dab007e
Vibhu pandey <vibhupandey.vp2gmail.com>
2021-07-12 19:51:34 +0530

    arhsa0

--- index.js
+++ index.js
@@ -599,8 +599,8 @@ async function main() {
                                 //     reply("These are the admin commands");
                                 //     return;
                                 // }
-                                let price = await getPrice(args[0]);
-                                reply(price);
+                                //let price = await getPrice(args[0]);
+                                reply(args[0]);
                                 break
                                  
 
