53f77cc5b7741ce2e4d4b78ff85534794e2668ad
Crysosancher <vibhupandey.vp@gmail.com>
2021-08-22 22:35:36 +0530

    extendtext

--- index.js
+++ index.js
@@ -477,7 +477,8 @@ async function main() {
                             // reply(taggy)
                             //reply(mesaj);
                             let tx = "xyz"
-                            await conn.sendMessage(from, mesaj, MessageType.extendedText,{ contextInfo: { mentionedJid: jids },previewType: 0 });
+                            await conn.sendMessage(from, mesaj, MessageType.extendedText)
+                            /* { contextInfo: { mentionedJid: jids },previewType: 0 });*/
                            //taggy(" ");
                         }
                         else {
